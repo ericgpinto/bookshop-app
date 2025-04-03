503f4e0dadd925dbe23176d06c459c8f62e8eaa3
ericgpinto <ericgrandopinto@gmail.com>
2025-04-03 19:51:36 -0300

    initial commit

---
+++ bookshop-service/src/main/java/com/ericpinto/bookshopservice/BookshopServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ericpinto.bookshopservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookshopServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookshopServiceApplication.class, args);
+    }
+
+}
