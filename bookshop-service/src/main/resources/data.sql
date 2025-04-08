2e425c4998e69c925ccc39f1c1ac968d34006ddb
ericgpinto <ericgrandopinto@gmail.com>
2025-04-08 15:59:58 -0300

    chore(sql): roda script para criação de livros em tempo de execução

---
+++ bookshop-service/src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO books (title, author, gender, year, is_rented) VALUES
+      ('Sapiens', 'Yuval Noah Harari', 'História', 2011, false),
+      ('1984', 'George Orwell', 'Ficção', 1949, false),
+      ('O Senhor dos Anéis', 'J.R.R. Tolkien', 'Fantasia', 1954, false),
+      ('Dom Casmurro', 'Machado de Assis', 'Romance', 1899, false),
+      ('A Revolução dos Bichos', 'George Orwell', 'Fábula política', 1945, false),
+      ('O Pequeno Príncipe', 'Antoine de Saint-Exupéry', 'Fábula', 1943, false),
+      ('Crime e Castigo', 'Fiódor Dostoiévski', 'Romance psicológico', 1866, false),
+      ('A Arte da Guerra', 'Sun Tzu', 'Estratégia', -500, false),
+      ('O Hobbit', 'J.R.R. Tolkien', 'Fantasia', 1937, false),
+      ('O Código Da Vinci', 'Dan Brown', 'Suspense', 2003, false),
+      ('Memórias Póstumas de Brás Cubas', 'Machado de Assis', 'Romance', 1881, false);
